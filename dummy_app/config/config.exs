370cf98a2e2179ef150c2ff47ff6b9fa8250992c
Enrique Fernandez <efcasado@gmail.com>
2021-10-02 15:09:01 +0200

    Update DummyApp

--- dummy_app/config/config.exs
+++ dummy_app/config/config.exs
@@ -2,7 +2,9 @@ use Mix.Config
 
 config :logger,
   backends: [:console],
-  format: {DummyApp.Logger.Formatter, :format},
   utc_log: true,
   handle_otp_reports: true,
   handle_sasl_reports: true
+
+config :logger, :console,
+  format: {DummyApp.Logger.Formatter, :format}
--- dummy_app/lib/dummy_app/logger/formatter.ex
+++ dummy_app/lib/dummy_app/logger/formatter.ex
@@ -1,10 +1,21 @@
 defmodule DummyApp.Logger.Formatter do
   def format(level, message, timestamp, metadata) do
+    __format__(level, message, timestamp, metadata)
+  rescue
+    _ ->
+      __format__(level, "#{inspect message}", timestamp, metadata)
+  end
+
+  defp __format__(level, message, timestamp, metadata) do
     %{
       level: level,
       timestamp: timestamp,
       metadata: metadata,
       message: message
-    } |> JSON.encode!
+    }
+    |> JSON.encode!
+    |> new_line()
   end
+
+  defp new_line(str), do: "#{str}\n"
 end
